63d962d58688c744e65b37103efdcc8b87a61d7a
MdShohanAli <720shohan@gmail.com>
2023-04-15 13:05:06 +0600

    remove await

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-        await client.connect();
+        client.connect();
         const productCollection = client.db('emaJohn').collection('product');
 
         app.get('/products', async (req, res) => {
